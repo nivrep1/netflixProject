243dcd743dba41ac8fcf07925dae08dbaeed47b3
WIN-E8402T24THH\Nivrep <oruczade1229@gmail.com>
2022-11-13 19:20:25 +0400

    main pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+//Pages Routing
+import Home from "./Pages/Home";
+import TvShows from "./Pages/tvShows";
+import Movies from "./Pages/Movies";
+import NewPopular from "./Pages/NewPopular";
+import MyList from "./Pages/MyList";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tvShows" element={<TvShows />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/newPopular" element={<NewPopular />} />
+        <Route path="/myList" element={<MyList />} />
+      </Routes>
+    </>
   );
 }
 
