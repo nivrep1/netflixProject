df763bc888df3897cbfc1580061557d2c0807523
WIN-E8402T24THH\Nivrep <oruczade1229@gmail.com>
2022-11-17 22:04:52 +0400

    Request problem

--- src/Components/Header/Search.jsx
+++ src/Components/Header/Search.jsx
@@ -4,7 +4,9 @@ import { HiSearch, HiBell } from "react-icons/hi";
 const Search = () => {
   return (
     <div className="flex items-center space-x-4 text-sm font-light">
+    
       <HiSearch className="text-white sm hidden h-6 w-6 sm:inline" />
+      
       <HiBell className="text-white h-6 w-6" />
     </div>
   );
--- src/Requests.js
+++ src/Requests.js
@@ -1,17 +1,17 @@
 // TMDB API KEYS && REQUESTS
 
-const apiKey = "5873e06f4a42d7c901d515b672defd63";
-const host = "https://api.themoviedb.org"
+const api_key = "5873e06f4a42d7c901d515b672defd63";
+const host = "https://api.themoviedb.org";
 
 const requests = {
   requestPopular: `${host}/3/movie/popular`,
-  requestTopRated: `${host}/3/movie/top_rated?${apiKey}&language=en-US&page=1`,
-  requestTrending: `${host}/3/movie/popular?${apiKey}&language=en-US&page=2`,
-  requestHorror: `${host}/3/search/movie?${apiKey}&language=en-US&query=horror&page=1&include_adult=false`,
-  requestUpcoming: `${host}/3/movie/upcoming?${apiKey}&language=en-US&page=1`,
-
-  request2: `${host}/3/movie/popular?api_key=5873e06f4a42d7c901d515b672defd63&language=en-US&page=1`
+  requestTrendAllWeek: `${host}/3/trending/all/week`,
+  requestUpcoming: `${host}/3/movie/upcoming`,
+  requestTvPopular: `${host}/3/tv/popular`,
+  requestTvAiringToday: `${host}/3/tv/airing_today`,
+  requestTvTopRated: `${host}/3/tv/top_rated`,
+  requestNetflixOriginals: `${host}/3/discover/tv`,
+  requestSearch: `${host}/3/search/movie`,
 };
 
 export default requests;
-
