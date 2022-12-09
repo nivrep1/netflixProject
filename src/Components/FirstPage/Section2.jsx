b90e04f364d911508a80f42959b1eda324ba7245
WIN-E8402T24THH\Nivrep <oruczade1229@gmail.com>
2022-12-09 20:21:37 +0400

    Image

---
+++ src/Components/FirstPage/Section2.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Section2 = () => {
+  return (
+    <div>
+      <div>
+        <h2>Enjoy on your TV.</h2>
+        <p>
+          Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
+          players, and more.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Section2;
