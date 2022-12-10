381de76aaccde6a26114149db6cea33128b06847
WIN-6LUN2D0CH7K\user <tofiqmvsm2003@gmail.com>
2022-12-11 01:35:12 +0400

    Footer Section

---
+++ src/Components/FirstPage/Footer.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import LanguageChange from './LanguageChange'
+import "../../styles/FirstPage/Footer.scss"
+
+const Footer = () => {
+  return (
+    
+    
+<div className='footer'>
+<h1><a href="#">Questions?Contact us.</a></h1>
+    <div className='big-div'>
+      <div className='small-divs'>
+        <ul>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Investor Relations</a></li>
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Speed Test</a></li>
+        </ul>
+      </div>
+      <div className='small-divs'>
+        <ul>
+          <li><a href="#">Help Center</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Cookie Preferences</a></li>
+          <li><a href="#">Legal Notices</a></li>
+        </ul>
+      </div>
+      <div className='small-divs'>
+        <ul>
+          <li><a href="#">Account</a></li>
+          <li><a href="#">Ways to Watch</a></li>
+          <li><a href="#">Corporate Information</a></li>
+          <li><a href="#">Only on Netflix</a></li>
+        </ul>
+      </div>
+      <div className='small-divs'>
+        <ul>
+          <li><a href="#">Media Center</a></li>
+          <li><a href="#">Terms of Use</a></li>
+          <li><a href="#">Contact Us</a></li>
+          
+        </ul>
+      </div>
+    </div>
+    <div className="real-footer">
+    <LanguageChange/>
+    <span className='footer-title'>Netflix Azerbaijan</span>
+    </div>
+    
+    </div>
+  
+   
+    
+  )
+}
+
+export default Footer
--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../styles/Header/logo.scss";
+
 
 const logo = ({ onClick }) => {
   return (
