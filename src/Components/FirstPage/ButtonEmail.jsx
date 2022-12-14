77bd3a1ef91c15530248c084ceccd0ada42629e9
WIN-6LUN2D0CH7K\user <tofiqmvsm2003@gmail.com>
2022-12-14 22:25:17 +0400

    ButtonEmail

---
+++ src/Components/FirstPage/ButtonEmail.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import "../../styles/FirstPage/buttonEmail.scss"
+
+const ButtonEmail = () => {
+  return (
+    <div className="button-email">
+    <p className="text">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+      <TextField
+        style={{ width: "55%" }}
+        required
+        InputLabelProps={{ style: { color: "#8c8c8c" } }}
+        sx={{ border: "none", backgroundColor: "#fff" }}
+        id="filled-basic"
+        label="Email"
+        variant="filled"
+        color="warning"
+        className="textField"
+      />
+      <Link to="/signup">
+      <button className="get-started"><a href="#">Get Started</a></button>
+      </Link>
+    </div>
+  );
+};
+
+export default ButtonEmail;
